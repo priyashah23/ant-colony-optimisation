b516636688ba92a162825be491d69e8cb9b0653e
Priya Shah <priya.shah2309@gmail.com>
2023-12-11 08:15:05 +0000

    Made a menu

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # README 
-The travelling salesman problem (TSP) is considered to be an "NP-Hard" problem. Ant Colony Optimisaton is a way to optimise the shortest number of paths.
+## Introduction 
+The travelling salesman problem (TSP) is considered to be an "NP-Hard" problem. This means that the solving the problem
+can take exponential time and with larger cities means it is basically impossible to find. 
+
+Ant Colony Optimisation is a Meta-Heuristic Functions whose goal it is to optimise the solution to find the shortest 
+path. This program allows a user to choose a variety of parameters. It prints out a graph showing the ant colonies 
+heuristic cost of the number of evaluations. The points contain the minimum cost that was obtained from the tour. 
+
+## Prerequisites
+Python 3.11 or higher\
+The files "brazil58" and "burma14" should be already in the repository they are required for this project to work.
+They can also be found on the ELE page.
+
+## Getting started
+**Ensure that the files brazil58 and burma14 are in the same directory as main.py for this to work**
+Open up a terminal of your choice and locate to directory where package was downloaded. 
+Simply run from terminal python main.py or if there are multiple versions of python are installed python3 main.py.
+
+### Navigating the menu 
+First the menu makes you a select a city choosing between either Brazil or Burma. Then the menu lets you select the 
+following parameters that may affect the behaviour of the ant colony. 
+- Number of Ants in a Colony
+- Decay Factor
+- Alpha 
+- Beta
+
+**Number of Ants In a Colony** - describes the number of ants that go on a tour per iteration [10, 50, 100]\
+**Decay Factor** - describes the rate that the pheromone will decay after a tour is complete [0.1, 0.5, 0.9] \
+**Alpha** - A constant factor that controls how much the pheromone influences the ants transition to the next path [0.5, 1, 3] \
+**Beta**  A constant that controls how much the heuristic controls the probability of the ants transition [1, 3, 5]\ 
+
+Once these parameters have been entered the program should run accordingly and print a graph that shows the number of
+evaluations and the heuristic function. 
--- antColonyOptimisation.py
+++ antColonyOptimisation.py
@@ -1,16 +1,12 @@
 import random
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 MAXIMUM_NUMBER_OF_FITNESS_EVALUATIONS = 10_000
-TEST_NUMBER_OF_ITERATIONS = 10
-TEST_SINGLE_ANT = 1
 
 
 class Ant:
     """
-    Class that h
+    Class that
     """
     def __init__(self, current_city: int, city_space: np.ndarray):
         """
@@ -25,8 +21,8 @@ class Ant:
 
     def remove_current_node(self, current_node: int) -> None:
         """
-        Removes the current city as a possible path for the ant to explore
-        :param current_node: Curreny city
+        Removes the current city as a possible path for the ant to explore in the adjacency matrix
+        :param current_node: Current city
         :return: Void
         """
         rows_to_update = np.arange(self.city_space.shape[0])
@@ -35,13 +31,13 @@ class Ant:
 
     def compute_transition_probabilities(self, current_node, tau, alpha, beta, t) -> np.ndarray:
         """
-        Computes the transition probabilties
+        Computes the transition probabilities
         :param current_node: the current city the ant is on
         :param tau: The pheromone matrix
         :param alpha: Alpha is a constant controlling how much of the pheromone is involved in the transition
-        :param beta: Beta is A constant controlling how much the heuristic influences the transitions
+        :param beta: Beta is a constant controlling how much the heuristic influences the transitions
         :param t: The current iteration
-        :return:
+        :return: a numpy array containing the probabilities of the ant visiting the next node
         """
         # Calculate Numerators
         numerators = tau[current_node] ** alpha * (self.city_space[current_node] ** beta) * t
@@ -52,16 +48,33 @@ class Ant:
 
         return probabilities
 
-    def update_memory(self, city):
+    def update_memory(self, city) -> None:
+        """
+        Adds the city to the ant's memory
+        :param city: The city the ant is going to visit
+        :return: None
+        """
         return self.memory.append(city)
 
-    def update_cost(self, current_city, next_city, adj_matrix: np.ndarray):
+    def update_cost(self, current_city, next_city, adj_matrix: np.ndarray) -> None:
+        """
+        This function updates the cost of the ants path from the current city to the next city
+        :param current_city: the city the ant is currently on
+        :param next_city: the next city the ant will visit
+        :param adj_matrix: takes the adjacency matrix to find what the cost is to visit the next city
+        :return: None
+        """
         if len(self.memory) == adj_matrix.shape[0]:
             first_element = self.memory[0] - 1
             self.cost += adj_matrix[next_city - 1][first_element]
         self.cost += adj_matrix[current_city - 1][next_city - 1]
 
     def deposit_pheromone(self, pheromone_matrix: np.ndarray) -> np.ndarray:
+        """
+        This function is where an ant deposit pheromones on every path that the ant visits
+        :param pheromone_matrix: The pheromone matrix that is being updated
+        :return: The updated pheromone matrix where the current ant has deposited its pheromone
+        """
         delta = 1/self.cost
         for x in range(len(self.memory)):
             if x == len(self.memory) - 1:
@@ -72,9 +85,18 @@ class Ant:
                 pheromone_matrix[self.memory[x] - 1][self.memory[x + 1] - 1] += delta
         return pheromone_matrix
 
-def ant_system_algorithm(number_of_ants: int, adj_matrix: np.ndarray, tau: np.ndarray, alpha, beta, decay_factor) -> list:
+def ant_system_algorithm(number_of_ants: int, adj_matrix: np.ndarray, tau: np.ndarray, alpha, beta, decay_factor, Q) -> list:
+    """
+    :param number_of_ants:
+    :param adj_matrix:
+    :param tau:
+    :param alpha:
+    :param beta:
+    :param decay_factor:
+    :return:
+    """
     list_of_points = []
-    heuristic_matrix = construct_heuristic_matrix(adj_matrix)
+    heuristic_matrix = construct_heuristic_matrix(adj_matrix, Q)
     number_of_fitness_evaluations = 0
     while number_of_fitness_evaluations <= MAXIMUM_NUMBER_OF_FITNESS_EVALUATIONS:
         # initialise ants here
@@ -87,43 +109,67 @@ def ant_system_algorithm(number_of_ants: int, adj_matrix: np.ndarray, tau: np.nd
         minimum_cost = min(ant_list, key=lambda x: x.cost)
         list_of_points.append((number_of_fitness_evaluations, minimum_cost.cost))
 
-        # Evaporate Pheromones
-        tau = evaporate_pheromones(decay_factor, tau)
+        # Deposit Pheromones
         for current_ant in ant_list:
             tau = current_ant.deposit_pheromone(tau)
+
+        # Evaporate Pheromones
+        tau = evaporate_pheromones(decay_factor, tau)
     return list_of_points
 
 
 
 def perform_ant_tour(length: int, current_ant: Ant, tau, alpha, beta, n, adj_matrix):
+    """
+
+    :param length:
+    :param current_ant:
+    :param tau:
+    :param alpha:
+    :param beta:
+    :param n:
+    :param adj_matrix:
+    :return:
+    """
     for _ in range(length):
         current_ant.remove_current_node(current_ant.current_city - 1)
         probabilities = current_ant.compute_transition_probabilities(current_ant.current_city - 1, tau, alpha, beta, n)
         select_next_node(current_ant, probabilities, adj_matrix)
 
-
-def apply_local_search():
-    pass
-
 def evaporate_pheromones(decay_factor, tau: np.ndarray) -> np.ndarray:
+    """
+
+    :param decay_factor:
+    :param tau:
+    :return:
+    """
     for i in range(tau.shape[0]):
         for j in range(tau.shape[1]):
                 tau[i][j] = (1 - decay_factor) * tau[i][j]
     return tau
 
-def construct_heuristic_matrix(adj_matrix: np.ndarray) -> np.ndarray:
+def construct_heuristic_matrix(adj_matrix: np.ndarray, Q: int) -> np.ndarray:
+    """
+    :param adj_matrix:
+    :return:
+    """
     eta = np.zeros((adj_matrix.shape[0], adj_matrix.shape[1]))
     for i in range(adj_matrix.shape[0]):
         for j in range(adj_matrix.shape[1]):
-            if i !=j :
-                eta[i][j] = round(1/adj_matrix[i][j], 4)
+            if i != j:
+                eta[i][j] = round(Q/adj_matrix[i][j], 4)
             else:
                 eta[i][j] = 0
 
     return eta
 
 def select_next_node(current_ant: Ant, probabilities: np.ndarray, adj_matrix):
-    # TODO - move into own method
+    """
+    :param current_ant:
+    :param probabilities:
+    :param adj_matrix:
+    :return:
+    """
     cumulative_probabilities = np.cumsum(probabilities)
     random_probability = random.uniform(0, 1)
     for x in range(len(cumulative_probabilities)):
